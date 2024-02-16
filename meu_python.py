03523aec2f49e9f1f463c6bf07745e77604a23df
Ricardo <ricardolc2@al.insper.edu.br>
2024-02-16 10:57:17 -0300

    a

--- meu_python.py
+++ meu_python.py
@@ -2,44 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-a = 3
-b = 2
-
-c = a + b
-
-print("olá mundo: ", c)
-
-
-print("olá mundo 4: ", c)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route('/a')
+def a():
+    return "<p>Oi plebe<p>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
